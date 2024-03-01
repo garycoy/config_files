18a200a52117561898e643dc580b0b415298a34d
Gary Coy <32619147+garycoy@users.noreply.github.com>
2024-03-01 14:43:26 -0600

    Update virtual_screen_setup.sh

--- virtual_screen_setup.sh
+++ virtual_screen_setup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-xrandr --setmonitor left 1918/535x1080/300+0+0 DP-1-2
+EXT_MONITOR=$1
+LAPTOP=eDP-1
+
+xrandr --output ${EXT_MONITOR} --primary
+
+xrandr --listactivemonitors
+
+xrandr --output ${LAPTOP} --off
+
+xrandr --setmonitor left 1918/535x1080/300+0+0 ${EXT_MONITOR}
 xrandr --setmonitor right 1918/535x1080/300+1919+0 none
 xrandr --fb 3840x1081
 xrandr --fb 3840x1080
+
+xrandr --output ${LAPTOP} --mode 1920x1080
