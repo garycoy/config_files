50ca9746a3915993d0e8335627dd90636ab3986d
Gary Coy <32619147+garycoy@users.noreply.github.com>
2024-03-01 17:02:18 -0600

    Create copy_config.sh

---
+++ copy_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# run as user1
+
+cp /user1_config/.config .
+cp -pR /user1_config/.config .
+cp -pR /user1_config/.oh-my-zsh .
+cp /user1_config/.zshrc .
+cp /user1_config/.Xauthority .
+cp /user1_config/pavucontrol.ini .
