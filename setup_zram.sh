3610fbd00bac5a8dd1a2beb61b89415e2301fecb
Gary Coy <32619147+garycoy@users.noreply.github.com>
2024-03-01 17:00:04 -0600

    Update and rename setup_zram.txt to setup_zram.sh

--- setup_zram.sh
+++ setup_zram.sh
@@ -1,4 +1,6 @@
 
+#/bin/bash
+
 # Set up zram mount for /home/user1
 # refer to: https://www.kernel.org/doc/html/latest/admin-guide/blockdev/zram.html
 #           https://wiki.archlinux.org/title/Zram
@@ -15,14 +17,9 @@ cat /sys/class/zram-control/hot_add
 zramctl /dev/zram1 --algorithm zstd --size 2G
 
 # check fdisk to make sure new device is visible
-fdisk -l
+# fdisk -l
 
 mkfs.ext4 /dev/zram1
 
 mount /dev/zram1 /home/user1
 chown user1:user1 /home/user1
-
-ln -s /user1_config/.config ~/.config
-ln -s /user1_config/.zshrc ~/.zshrc
-ln -s /user1_config/.oh-my-zsh ~/.oh-my-zsh
-ln -s /user1_config/.Xauthority ~/.Xauthority
